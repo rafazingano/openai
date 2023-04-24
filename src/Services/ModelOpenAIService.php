ab5afd3304e4b7fa457230893cee58bc5bc03e98
rafazingano <rafazingano@gmail.com>
2023-04-24 00:10:50 -0300

    Ajuste composer

--- src/Services/ModelOpenAIService.php
+++ src/Services/ModelOpenAIService.php
@@ -7,14 +7,14 @@ class ModelOpenAIService extends OpenAIService
 
     public function list()
     {
-        $response = $this->client->get('/models');
+        $response = $this->client->get('models');
 
         return $response;
     }
 
     public function retrieve($modelId)
     {
-        $response = $this->client->get('/models/' . $modelId);
+        $response = $this->client->get('models/' . $modelId);
 
         return $response;
     }
